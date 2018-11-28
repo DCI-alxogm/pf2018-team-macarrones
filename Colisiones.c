1a3f9cf2309be13cf543298acac3a56886b802d0
Herrera Mosqueda Mariana <herreramm2018@dci25-178.ugto.mx>
2018-11-28 09:57:49 -0600

    inicio de nuestro proyecto final, se continuara despues

---
+++ Colisiones.c
@@ -0,0 +1,11 @@
+/*Se inicia el proyecto de final de colision de galaxias el dia 28 de noviembre, integrantes: Haydeé Preciado y Mariana Herrera, se continuara en los dias siguientes*/
+
+#include<stdio.h>
+#include<math.h>
+int main()
+FILE *datos;
+FILE *nuevos-datos;
+{
+datos=fopen("datos.txt",r);
+}
+return 0;
