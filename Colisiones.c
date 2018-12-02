7bf8d16565c2a4ae0037e39a76a2e6c3ad949593
mariana-E402SA <marianaherreramosuqeda@gmail.com>
2018-12-02 17:53:10 -0600

    avanzar proyecto

--- Colisiones.c
+++ Colisiones.c
@@ -3,9 +3,27 @@
 #include<stdio.h>
 #include<math.h>
 int main()
-FILE *datos;
-FILE *nuevos-datos;
 {
-datos=fopen("datos.txt",r);
+FILE *datos;
+FILE *datos2;
+int i;
+float pi,G,F,r,x,y,z,vx,vy,vz,h,m,t;
+pi=3.1416;
+G=4*pow(pi,2);
+datos=fopen("datos.txt","r");
+scanf("%f",&m,&t,&x,&y,&z,&vx,&vy,&vz,&h);
+
+/*for (j=0; j<10; j++);
+{
+	r=sqrt(pow(x2-x1,2)-pow(y2-y1,2)-pow(z2-z1,2));
+}*/
+
+for(i=0; i<10 ; i++);
+{
+	F=-(G(m*m)/pow(r,3));
 }
+printf("%f\n",r);
+fclose(datos);
+
 return 0;
+}
