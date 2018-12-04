b7e0fc453776722309d3f897ebd11c383574fe9c
Herrera Mosqueda Mariana <herreramm2018@dci25-178.ugto.mx>
2018-12-04 11:21:02 -0600

    vanzamos aunque se generaron varias dudas

--- Colisiones.c
+++ Colisiones.c
@@ -1,4 +1,4 @@
-/*Se inicia el proyecto de final de colision de galaxias el dia 28 de noviembre, integrantes: Haydeé Preciado y Mariana Herrera, se continuara en los dias siguientes*/
+//Se inicia el proyecto de final de colision de galaxias el dia 28 de noviembre, integrantes: Haydeé Preciado y Mariana Herrera, se continuara en los dias siguientes
 
 #include<stdio.h>
 #include<math.h>
@@ -6,46 +6,61 @@ int main()
 {
 FILE *datos;
 FILE *datos2;
-int i,j;
-int n=20;
-float F,r,h,m,t;
+int i,r;
+double np=20;                                                
+float F,h,mp,t,m,R;
 float xi,yi,zi,vxi,vyi,vzi,Fx,Fy,Fz;
 float pi=3.1416;
 float G=4*pow(pi,2);
 
-printf("Ingrese el radio de su preferencia para la esfera: \n");
-scanf("%i",&r);
-
 datos=fopen("datos.txt","r");
-scanf("%f",&m,&t,&xi,&yi,&zi,&vxi,&vyi,&vzi,&h);
+scanf("%f",&mp,&t,&xi,&yi,&zi,&vxi,&vyi,&vzi,&h);
+fclose(datos);
+datos2=fopen("datos2.txt","w");
 
- for (j=0; j<n ;j++);
+printf("Introduce un radio entero deseado: \n");
+scanf("%i", &r);
+
+//Calcular la masa de cada galaxia
+//Se hace un solo ciclo porque la masa de las particulas es la misma y la cantidad de particulas de cada esfera son las mismas
+
+for(i=0; i<np; i++);
 {
-	xi=((r)*(x*cos(18)));
-	yi=((r)*(y*sin(18)));
-	printf("%f",&xi,&yi);
-}
+	m+=mp;
+}	
 
-	/*r=sqrt(pow(x2-x1,2)+pow(y2-y1,2)+pow(z2-z1,2));*/
 
-/*
-for(i=0; i<10 ; i++);
+//Calcular la fuerza
+
+F=-(G(m*m)/pow(r,3));
+
+
+
+//Ciclo para calcular la distancia de la galaxia
+for(i=0; i<np; i++);
 {
  	
-	F=-(G(m*m)/pow(r,3));
-}
-*/
-
-/*Ciclo para poder caclular la nueva posicion y la nueva velocidad*/
-for (m=0, m<p, m++);{
-	xi(t+h)=xi(t)+h*vxi(t)+1/2*Fx*pow(h,2);
-	yi(t+h)=yi(t)+h*vyi(t)+1/2*Fy*pow(h,2);
-	zi(t+h)=zi(t)+h*vzi(t)+1/2*Fz*pow(h,2);
+	R=pow(-G*(m*m)/F,1/3);
+	
 }
 
 
-printf("%f\n",r);
-fclose(datos);
+//Ciclo para poder calcular la nueva posicion y la nueva velocidad
+for (i=0; i<np; i++);
+{
+	xi(t+h)=xi*(t)+h*vxi*(t)+1/2*Fx*pow(h,2);
+	yi(t+h)=yi*(t)+h*vyi*(t)+1/2*Fy*pow(h,2);
+	zi(t+h)=zi*(t)+h*vzi*(t)+1/2*Fz*pow(h,2);
+}
+for(i=0; i<np; i++);
+{
+	vxi(t+h)=vxi*(t)+1/2(Fx*(t)+Fx(t+h))*h;
+	vyi(t+h)=vyi*(t)+1/2(Fy*(t)+Fy(t+h))*h;
+	vzi(t+h)=vzi*(t)+1/2(Fz*(t)+Fz(t+h))*h;
+}
+
+fprintf(datos2,"%f,%f,%f,%f,%f,%f", xi, yi, zi, vxi, vyi, vzi); //Se impren los nuevos datos en un archivo separado
+fclose(datos2);
 
 return 0;
 }
