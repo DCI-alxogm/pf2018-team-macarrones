6d411ce97a66da445c8b2524d84a08841165688f
Herrera Mosqueda Mariana <herreramm2018@dci25-178.ugto.mx>
2018-12-06 09:56:50 -0600

    avanzar

--- funcposicion.c
+++ funcposicion.c
@@ -4,26 +4,32 @@
 #include<stdlib.h>
 int main ()
 {
-int np=50;
-float xi[np] ,vxi[np],fxi[np],h[np],t[np],x[np];
+
+int i,np=50; 
+float xi[np], vxi[np], fxi[np], h[np], t[np], x[np], y[np], yi[np], vyi[np], fyi[np], z[np], zi[np], vzi[np], fzi[np];
 FILE* datos;
 FILE* datos2;
-FILE *posicionesN;
+FILE* posicionesN;
 
 datos=fopen("datos.txt", "r");
-fscanf(datos, "%f,%f,%f",&xi, &vxi,&fxi);
+fscanf(datos, "%f,%f,%f,%f,%f,%f,%f,%f,%f",&xi,&vxi,&fxi,&yi,&vyi,&fyi,&zi,&vzi,&fzi);
+fclose(datos);
+
 datos2=fopen("constantes.txt", "r");
 fscanf(datos2, "%f, %f", &h, &t);
+fclose(datos2);
 
 posicionesN=fopen("posicionesN.txt","w");
 
-for(i=0,i<np,i++);{
-	x=(xi*(t)+(h*vxi*t)+((1/2)*fxi*(pow(h,2))))/(t+h);
-	y=(yi*(t)+(h*vyi*t)+((1/2)*fyi*(pow(h,2))))/(t+h);
-	z=(zi*(t)+(h*vzi*t)+((1/2)*fzi*(pow(h,2))))/(t+h);
+for(i=0;i<np;i++);
+{
+	
+	x=(xi*(t)+(h*vxi*t)+((1/2)*fxi*(h*h))/(t+h));
+	y=(yi*(t)+(h*vyi*t)+((1/2)*fyi*(h*h))/(t+h));
+	z=(zi*(t)+(h*vzi*t)+((1/2)*fzi*(h*h))/(t+h));
 }
 
-fprintf(posicionesN,"%f\n", &x);
+fprintf(posicionesN,"%f\n", &x,&z,&y);
 fclose(posicionesN);
 return 0;
 }
