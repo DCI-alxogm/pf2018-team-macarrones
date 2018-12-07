0f865086073cb2b6271cc08e2e25eba86b4c7cd6
mhm1610 <42146384+mhm1610@users.noreply.github.com>
2018-12-07 12:50:35 -0600

    Avanzado

--- esferas.c
+++ esferas.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 #include<math.h>
-#include<string.h>
+#include<stdlib.h>
+#include<time.h>
 
 int main()
 {
+int num;
+FILE* datos;
+datos=fopen("cooresferas.txt", "w");
+srand(time(NULL));
+num=rand()%51;
+fprintf(datos, "%d\n", num);
+fclose(datos);
+return 0;
 
-int crea (int M){
-int pos=rand();
-pos%=2;
-int num=rand();
-int rest=num/(M*10000);
-num-=rest*(M*10000);
-num /=10000;
-return num;
-}
 }
